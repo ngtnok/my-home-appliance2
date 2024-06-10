3547ad0c6eb18672626b921c25e47323bf1d97e7
ngtnok <105690483+ngtnok@users.noreply.github.com>
2024-06-10 15:09:34 +0900

    add: test

--- src/test/kotlin/com/example/myHomeAppliance2/MyHomeAppliance2ApplicationTests.kt
+++ src/test/kotlin/com/example/myHomeAppliance2/MyHomeAppliance2ApplicationTests.kt
@@ -1,13 +1,25 @@
 package com.example.myHomeAppliance2
 
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.http.HttpStatus
 
-@SpringBootTest
-class MyHomeAppliance2ApplicationTests {
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class MyHomeAppliance2ApplicationTests(
+	@Autowired val restTemplate: TestRestTemplate,
+	@LocalServerPort val port: Int
+) {
 	@Test
 	fun contextLoads() {
 	}
-
+	@Test
+	fun `GET-api-appliances-idsリクエストに200を返す`(){
+		val response = restTemplate.getForEntity("http://localhost:$port/todos", String::class.java)
+		assertThat(response.statusCode, equalTo(HttpStatus.OK))
+	}
 }
