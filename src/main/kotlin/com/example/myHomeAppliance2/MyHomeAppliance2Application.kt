8f03a19250d2b55b7219010cd01d90b02c44c45f
ngtnok <105690483+ngtnok@users.noreply.github.com>
2024-06-10 11:04:31 +0900

    setup: spring-boot,vite

---
+++ src/main/kotlin/com/example/myHomeAppliance2/MyHomeAppliance2Application.kt
@@ -0,0 +1,11 @@
+package com.example.myHomeAppliance2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MyHomeAppliance2Application
+
+fun main(args: Array<String>) {
+	runApplication<MyHomeAppliance2Application>(*args)
+}
---
+++ src/test/kotlin/com/example/myHomeAppliance2/MyHomeAppliance2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.myHomeAppliance2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MyHomeAppliance2ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
