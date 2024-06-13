c149332438d0d9b0093252e86ae7b398c4c94241
ngtnok <105690483+ngtnok@users.noreply.github.com>
2024-06-13 22:35:10 +0900

    add: frontend userComments on Details

--- frontend/src/components/Details.jsx
+++ frontend/src/components/Details.jsx
@@ -14,6 +14,7 @@ function Details({ allIds, selectedId, view, familyId, setView ,reload }) {
     const inputBuyDate = useRef("")
     const inputBuyAt = useRef("")
     const [detailsObj, setDetailsObj] = useState({});
+    const [comments, setComments] = useState([])
     const [usePlaces,setUsePlace] = useState([]);
     const [arrMaker, setMaker] = useState([
 "SHARP",
@@ -64,15 +65,13 @@ function Details({ allIds, selectedId, view, familyId, setView ,reload }) {
         inputBuyAt.current.value = detailsObj.buyAt || "";
         },[detailsObj])
     useEffect(()=>{
+        fetch(`/api/comments/${selectedId}`).then(res=> res.json() ).then(jsoned=>setComments(jsoned) ).catch(err=> console.error(err) )
+        },[])
+    useEffect(()=>{
         fetch("/api/use_places").then(res=>res.json()).then(jsoned => setUsePlace(jsoned)).catch(err=>console.error(err))
         },[])
     const urlhavings = "/api/havings";
     const funFetch = postOrPatch => {
-//         console.log({familyId});
-//         console.log({appId:selectedId});
-//         console.log({usePlace:inputUsePlace.current.value});
-//         console.log({buyDate:Number(new Date(inputBuyDate.current.value))});
-//         console.log({buyAt:inputBuyAt.current.value});
         fetch("/api/havings",{
             method: postOrPatch,
             headers: {
@@ -90,21 +89,7 @@ function Details({ allIds, selectedId, view, familyId, setView ,reload }) {
         reload(prev => prev + 1)
         }
     const onSubmit = () => {
-//         console.log("青いSAVEボタン")
         funFetch("PATCH")
-//         fetch(urlhavings,{
-//                 method: "PATCH",
-//                 headers: {
-//                     "Content-Type": "application/json",
-//                 },
-//                 body: JSON.stringify({
-//                     familyId,
-//                     appId:detailsObj.appId,
-//                     usePlace:inputUsePlace.current.value,
-//                     buyDate:Number(new Date(inputBuyDate.current.value)),
-//                     buyAt:inputBuyAt.current.value
-//                 })
-//             }).then(res=>res.json()).then(jsoned=> console.log(jsoned)).catch(err=>console.error(err))
         }
     const clickGet = () => {
         console.log("ようこそ")
@@ -147,8 +132,6 @@ function Details({ allIds, selectedId, view, familyId, setView ,reload }) {
           <Form.Label htmlFor="disabledTextInput">型番号</Form.Label>
           <Form.Control id="disabledTextInput" disabled placeholder={detailsObj.modelNumber} ref={inputModel}/>
         </Form.Group>
-        {/**{view === "editView" && ( */}
-            <>
          <Form.Group className="mb-3">
            <Form.Label htmlFor="disabledSelect">使用場所</Form.Label>
            <Form.Select id="disabledSelect" ref={inputUsePlace}>
@@ -164,18 +147,32 @@ function Details({ allIds, selectedId, view, familyId, setView ,reload }) {
           <Form.Label htmlFor="disabledTextInput">購入元</Form.Label>
           <Form.Control id="disabledTextInput" ref={inputBuyAt}/>
         </Form.Group>
-         </>
-           {/** )} */}
         </Form>
+                <Card>
+                  <Card.Header>使ってるみんなの声</Card.Header>
+        {comments.map(obj=> (
+                  <Card.Body>
+                    <blockquote className="blockquote mb-0">
+                      <p>
+                        {' '}
+                        {obj.comment}{' '}
+                      </p>
+                      <footer className="blockquote-footer">
+                        使用歴{Math.floor((obj.postDate-obj.buyDate)/(365*24*60*60*1000))}年
+                      </footer>
+                    </blockquote>
+                  </Card.Body>
+            ))}
+                </Card>
         {!detailsObj.appId ? (
-      <Card className="text-center my-5">
-            <Button variant="primary" onClick={clickGet}>ようこそ</Button>
-      </Card>
-            ):(
-      <Card className="text-center my-5">
-        <Button variant="outline-secondary" onClick={clickDelete}>今までありがとう</Button>
-      </Card>
-      )}
+            <Card className="text-center my-5">
+                <Button variant="primary" onClick={clickGet}>ようこそ</Button>
+            </Card>
+        ):(
+            <Card className="text-center my-5">
+                <Button variant="outline-secondary" onClick={clickDelete}>今までありがとう</Button>
+            </Card>
+        )}
     </>
   );
 }
--- frontend/src/components/HeaderMenu.jsx
+++ frontend/src/components/HeaderMenu.jsx
@@ -4,9 +4,6 @@ function HeaderMenu({setView,setId,view}) {
   return (
     <Nav justify variant="tabs" defaultActiveKey="/home">
       <Nav.Item>
-          <Nav.Link eventKey="link-1" onClick={()=>  setView("viewHistory")}>みんなの声</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
         <Nav.Link href="/home">ホーム</Nav.Link>
       </Nav.Item>
       <Nav.Item>
